4ecf184affc4f89146cdb3d822dd2bd890b06755
LuisFRL01 <luisfernando.25@hotmail.com>
2020-09-19 21:21:48 -0300

    Criar model e migrate Avaliacao

---
+++ app/Models/Avaliacao.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Avaliacao extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nota', 'texto',
+    ];
+
+    public function cliente(){
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function vendedor(){
+        return $this->belongsTo('App\Models\User');
+    }
+}
