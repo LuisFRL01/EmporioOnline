5b037fb47167629a1fe4e3a213be95d305f9bfd9
Edgar Vinicius Carvalho Vital <edgar.tecnico@hotmail.com>
2020-09-25 08:32:48 -0300

    Update CategoriaValidator.php

--- app/Validator/CategoriaValidator.php
+++ app/Validator/CategoriaValidator.php
@@ -8,7 +8,7 @@ class CategoriaValidator
 {
     public static function validate($data){
         $validator = \Validator::make($data, \App\Models\Categoria::$rules, \App\Models\Categoria::$messages);
-        if($validator->errors()->isEmpty())
+        if(!$validator->errors()->isEmpty())
             throw new ValidationException($validator, "Erro na validação da Categoria");
         return $validator;
     }
