90166dc2d038dbabd6eb2585d93712b83c5ad646
Edgar Vinicius Carvalho Vital <edgar.tecnico@hotmail.com>
2020-09-25 14:41:47 -0300

    Update DenunciaValidator.php

--- app/Validator/DenunciaValidator.php
+++ app/Validator/DenunciaValidator.php
@@ -3,11 +3,13 @@
 
 namespace App\Validator;
 
+use Illuminate\Support\Facades\Validator;
+
 
 class DenunciaValidator
 {
     public static function validate($data){
-        $validator = \Validator::make($data, \App\Models\Denuncia::$rules, \App\Models\Denuncia::$messages);
+        $validator = Validator::make($data, \App\Models\Denuncia::$rules, \App\Models\Denuncia::$messages);
         if(!$validator->errors()->isEmpty())
             throw new ValidationException($validator, "Erro na validação da Denuncia");
         return $validator;
