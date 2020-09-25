ee00df568ed5ed34788617f4641aec35f9ffcd7a
Edgar Vinicius Carvalho Vital <edgar.tecnico@hotmail.com>
2020-09-25 08:33:34 -0300

    Update ProdutoValidator.php

--- app/Validator/ProdutoValidator.php
+++ app/Validator/ProdutoValidator.php
@@ -8,7 +8,7 @@ class ProdutoValidator
 {
     public static function validate($data){
         $validator = \Validator::make($data, \App\Models\Produto::$rules, \App\Models\Produto::$messages);
-        if($validator->errors()->isEmpty())
+        if(!$validator->errors()->isEmpty())
             throw new ValidationException($validator, "Erro na validação do Produto");
         return $validator;
     }
