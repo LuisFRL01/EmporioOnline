1302730a8158e76a7a90ec9849a4415d1e6c85ea
Edgar Vinicius Carvalho Vital <edgar.tecnico@hotmail.com>
2020-09-25 08:32:05 -0300

    Update AdministradorValidator.php

--- app/Validator/AdministradorValidator.php
+++ app/Validator/AdministradorValidator.php
@@ -6,7 +6,7 @@ class AdministradorValidator
 {
 	public static function validate($data){
 		$validator = \Validator::make($data, \App\Models\Administrador::$rules, \App\Models\Administrador::$messages);
-		if($validator->errors()->isEmpty())
+		if(!$validator->errors()->isEmpty())
 			throw new ValidationException($validator, "Erro na validação do Administrador");
 		return $validator;
 	}
