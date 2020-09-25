21ee270686820437a0adce1dabcbc678cd7467dc
Edgar Vinicius Carvalho Vital <edgar.tecnico@hotmail.com>
2020-09-25 08:32:35 -0300

    Update CaracteristicaValidator.php

--- app/Validator/CaracteristicaValidator.php
+++ app/Validator/CaracteristicaValidator.php
@@ -8,7 +8,7 @@ class CaracteristicaValidator
 {
     public static function validate($data){
         $validator = \Validator::make($data, \App\Models\Caracteristica::$rules, \App\Models\Caracteristica::$messages);
-        if($validator->errors()->isEmpty())
+        if(!$validator->errors()->isEmpty())
             throw new ValidationException($validator, "Erro na validação da Caracteristica");
         return $validator;
     }
