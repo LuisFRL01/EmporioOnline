0fef77ec5c5045e7428a2d81c620414b8534ee1b
Edgar Vinicius Carvalho Vital <edgar.tecnico@hotmail.com>
2020-09-25 08:33:48 -0300

    Update UsuarioValidator.php

--- app/Validator/UsuarioValidator.php
+++ app/Validator/UsuarioValidator.php
@@ -8,7 +8,7 @@ class UsuarioValidator
 {
     public static function validate($data){
         $validator = \Validator::make($data, \App\Models\Usuario::$rules, \App\Models\Usuario::$messages);
-        if($validator->errors()->isEmpty())
+        if(!$validator->errors()->isEmpty())
             throw new ValidationException($validator, "Erro na validação do Usuário");
         return $validator;
     }
