206ff048a70f1e2ff6345447573f58f1c40925e1
Edgar Vinicius Carvalho Vital <edgar.tecnico@hotmail.com>
2020-09-25 08:32:20 -0300

    Update AvaliacaoValidator.php

--- app/Validator/AvaliacaoValidator.php
+++ app/Validator/AvaliacaoValidator.php
@@ -7,7 +7,7 @@ class AvaliacaoValidator
 {
     public static function validate($data){
         $validator = \Validator::make($data, \App\Models\Avaliacao::$rules, \App\Models\Avaliacao::$messages);
-        if($validator->errors()->isEmpty())
+        if(!$validator->errors()->isEmpty())
             throw new ValidationException($validator, "Erro na validação da Avaliação");
         return $validator;
     }
