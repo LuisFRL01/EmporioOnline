0a49b9ccf27822d16f3bd819404eb82cd90ac09a
Edgar Vinicius Carvalho Vital <edgar.tecnico@hotmail.com>
2020-09-25 08:33:19 -0300

    Update PedidoValidator.php

--- app/Validator/PedidoValidator.php
+++ app/Validator/PedidoValidator.php
@@ -8,7 +8,7 @@ class PedidoValidator
 {
     public static function validate($data){
         $validator = \Validator::make($data, \App\Models\Pedido::$rules, \App\Models\Pedido::$messages);
-        if($validator->errors()->isEmpty())
+        if(!$validator->errors()->isEmpty())
             throw new ValidationException($validator, "Erro na validação do Pedido");
         return $validator;
     }
