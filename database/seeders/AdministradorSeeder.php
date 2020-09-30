65c0a02f552e30254f96bee0b45c77eeede39fda
Edgarvital <edgar.tecnico@hotmail.com>
2020-09-30 18:16:21 -0300

    Factory e Seeder do Usuario

--- database/seeders/AdministradorSeeder.php
+++ database/seeders/AdministradorSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Administrador;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class AdministradorSeeder extends Seeder
 {
@@ -15,9 +15,6 @@ class AdministradorSeeder extends Seeder
 
     public function run()
     {
-        for($i = 0; $i < 10; $i++){
-            DB::table('administradors')->insert(['cpf' => rand(10000000000, 99999999999), 'nome' => 'Edgar',
-                'email' => 'edgar.carvalho' . $i . '@hotmail.com', 'senha' => 'password']);
-        }
+        Administrador::factory()->count(50)->create();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(AdministradorSeeder::class);
+        $this->call(UsuarioSeeder::class);
     }
 }
