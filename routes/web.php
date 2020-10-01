31b50e74d3a7085a115d6816be4c3892fac05b6f
LuisFRL01 <luisfernando.25@hotmail.com>
2020-10-01 16:00:49 -0300

    Instalar jetStream

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/teste', function () {
 Route::get('/', function () {
     return view('hello');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
