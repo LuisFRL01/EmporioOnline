9d891e9f25885bfa1ba9371acec1c8b9065e1e93
Edgar Vinicius Carvalho Vital <edgar.tecnico@hotmail.com>
2020-10-04 14:53:06 -0300

    Update JetstreamServiceProvider.php

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -44,7 +44,7 @@ class JetstreamServiceProvider extends ServiceProvider
         Jetstream::defaultApiTokenPermissions(['read']);
 
         Jetstream::permissions([
-            'produto',
+            'create',
             'read',
             'update',
             'delete',
